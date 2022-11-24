43847886e615e896f974d45e0028d5de08972a49
drojas10 <drojas10@bancoestado.cl>
2022-11-24 16:50:37 -0300

    firebase config ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ingreso-egreso-app-d1bca',
+    appId: '1:161757614022:web:b17bc0948a457ad7f8ecc7',
+    storageBucket: 'ingreso-egreso-app-d1bca.appspot.com',
+    apiKey: 'AIzaSyAlP4vQ_tc7ElPcnmskJShjsWM6_cpvXGM',
+    authDomain: 'ingreso-egreso-app-d1bca.firebaseapp.com',
+    messagingSenderId: '161757614022',
+    measurementId: 'G-E8PQ5E1BTM',
+  },
   production: false
 };
 
